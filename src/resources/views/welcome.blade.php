65b0ecae1cc7902fbbac548ef4641bf6694711b9
root <root@LAPTOP-FE1F6J5C.localdomain>
2025-07-21 11:27:00 +0700

    testing

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -43,6 +43,7 @@
             @endauth
         </nav>
     @endif
+    <p>test 123</p>
 </header>
 <div
     class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
